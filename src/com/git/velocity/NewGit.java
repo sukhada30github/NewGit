44918b7cc354ae276636bcf772e93c567e3b2129
Sukhada Wagale <sukhadawagale@gmail.com>
2023-07-31 11:34:18 +0530

    newgit is added into local repository

---
+++ src/com/git/velocity/NewGit.java
@@ -0,0 +1,68 @@
+package com.git.velocity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NewGit {
+
+	public static void main(String[] args) {
+		{
+			
+			ArrayList<String> annimation_teachers_list = new ArrayList<>();
+			
+			annimation_teachers_list.add("Shetty");
+			annimation_teachers_list.add("Talwar");
+			annimation_teachers_list.add("Bhosle");
+			
+			ArrayList<String> web_teachers_list = new ArrayList<>();
+			
+			web_teachers_list.add("Shinde");
+			web_teachers_list.add("Ingle");
+			web_teachers_list.add("Pisal");
+			
+			ArrayList<String> AI_teacher_list = new ArrayList<>();
+			
+			AI_teacher_list.add("Chamle");
+			AI_teacher_list.add("Kasle");
+			AI_teacher_list.add("Dounkar");
+			
+			HashMap<String, ArrayList<String>> Department = new HashMap<>();
+			
+			Department.put("Annimation", annimation_teachers_list);
+			Department.put("Web",  web_teachers_list);
+			Department.put("AI",AI_teacher_list );
+			
+			
+			
+			HashMap<String, HashMap<String, ArrayList<String>>> collage = new HashMap<>();
+			collage.put("Rammayya Collage", Department);
+			
+			Set<String> collkey = collage.keySet();
+			
+			Iterator<String> itr = collkey.iterator();
+			
+			for(String str : collkey)
+			{
+				System.out.println("Collage" + str);
+			}
+			
+		Set<String> depkey = Department.keySet();
+			
+			Iterator<String> itr2 = depkey.iterator();
+			
+			while(itr2.hasNext())
+			{
+				String next2=itr2.next();
+				System.out.println("Department  :"  + next2);
+				System.out.println("Teacher Name  :"  + Department.get(next2));
+			}
+			
+		}
+			
+		}
+
+	}
+
+
